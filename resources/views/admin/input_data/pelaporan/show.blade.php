3efc133d610897fddc032a4643c08c8700f62a96
DBMPR-JABAR <dbmpr.jabar.prov@gmail.com>
2022-06-30 16:08:26 +0700

    maps

--- resources/views/admin/input_data/pelaporan/show.blade.php
+++ resources/views/admin/input_data/pelaporan/show.blade.php
@@ -2,31 +2,115 @@
 
 @section('title') Edit pelaporan @endsection
 @section('head')
-<script src="http://maps.googleapis.com/maps/api/js"></script>
+<link rel="stylesheet" href="https://js.arcgis.com/4.24/esri/themes/light/main.css">
+<script src="https://js.arcgis.com/4.24/"></script>
+<style>
+   
+    #viewDiv {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        width: 100%;
+      }
+</style>
+
+
 <script>
-function initialize() {
     var lapor = {!! json_encode($pelaporan->toArray()) !!};
-    console.log(lapor);
-  var propertiPeta = {
-    center:new google.maps.LatLng(lapor.lat,lapor.long),
-    zoom:9,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  
-  var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
-  
-  // membuat Marker
-  var marker=new google.maps.Marker({
-      position: new google.maps.LatLng(lapor.lat,lapor.long),
-      map: peta,
-      animation: google.maps.Animation.BOUNCE
-  });
+ 
+    require([
+      "esri/config",
+      "esri/Map",
+      "esri/views/MapView",
 
-}
+      "esri/widgets/BasemapToggle",
+      "esri/widgets/BasemapGallery",
+      "esri/Graphic",
+      "esri/layers/GraphicsLayer"
 
-// event jendela di-load  
-google.maps.event.addDomListener(window, 'load', initialize);
+    ], function(esriConfig, Map, MapView, BasemapToggle, BasemapGallery, Graphic, GraphicsLayer) {
+
+        esriConfig.apiKey = "AAPK2021e3c0ade243ac91fc03c5cc16af553UoLz7PP3cuznJsJw2hQOU6G-m47W2PWSfHujOs9JYI-UmZOtUw7TvgwWHUSIDPI";
+
+        const map = new Map({
+            basemap: "arcgis-topographic"
+        });
+
+        const view = new MapView({
+            container: "viewDiv",
+            map: map,
+            center: [lapor.long, lapor.lat],
+            zoom: 9
+        });
+
+        const basemapToggle = new BasemapToggle({
+            view: view,
+            nextBasemap: "arcgis-imagery"
+        });
+
+        view.ui.add(basemapToggle,"bottom-right");
+
+        const basemapGallery = new BasemapGallery({
+            view: view,
+            source: {
+            query: {
+                title: '"World Basemaps for Developers" AND owner:esri'
+            }
+            }
+        });
+
+        view.ui.add(basemapGallery, "top-right"); // Add to the view
+        
+        const graphicsLayer = new GraphicsLayer();
+        map.add(graphicsLayer);
+        const point = { //Create a point
+            type: "point",
+            longitude: lapor.long,
+            latitude: lapor.lat
+        };
+        const simpleMarkerSymbol = {
+            type: "simple-marker",
+            color: [226, 119, 40],  // Orange
+            outline: {
+                color: [255, 255, 255], // White
+                width: 1
+            }
+        };
+        const pointGraphic = new Graphic({
+            geometry: point,
+            symbol: simpleMarkerSymbol
+        });
+        graphicsLayer.add(pointGraphic);
+
+    });
+
 </script>
+
+{{-- <script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+    function initialize() {
+        var lapor = {!! json_encode($pelaporan->toArray()) !!};
+        console.log(lapor);
+    var propertiPeta = {
+        center:new google.maps.LatLng(lapor.lat,lapor.long),
+        zoom:9,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    
+    var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+    
+    // membuat Marker
+    var marker=new google.maps.Marker({
+        position: new google.maps.LatLng(lapor.lat,lapor.long),
+        map: peta,
+        animation: google.maps.Animation.BOUNCE
+    });
+
+    }
+
+    // event jendela di-load  
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script> --}}
 @endsection
 @section('page-header')
 <div class="row align-items-end">
@@ -106,7 +190,9 @@ google.maps.event.addDomListener(window, 'load', initialize);
                         <label class="col-md-12 col-form-label">{{$pelaporan->long}}</label>
                     </div>
                 </div>
-                <div id="googleMap" style="width:100%;height:380px;"></div>
+                {{-- <div id="googleMap" style="width:100%;height:380px;"></div> --}}
+                <div id="viewDiv" style="width:100%;height:380px;"></div>
+
             </div>
         </div>
      
